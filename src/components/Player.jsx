8338e6fcd5364103731877078bf7eefe9805cb89
juankofla <juankofla@gmail.com>
2024-11-07 18:00:04 -0500

    I created new state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,18 +1,18 @@
 import { useState } from "react"
 
-const Player = ({name, symbol}) => {
+const Player = ( {initialName, symbol} ) => {
 
+    const [playerName, setPlayerName] = useState(initialName)
     const [isEditing, setIsEditing] = useState(false)
 
+    console.log(playerName);
+
     const handleEditName = () => {
-
         // if your new state depends on your previous state value, you should not update the state like this
-        setIsEditing(!isEditing)
-
+        setIsEditing((isEditing ) => !isEditing)
     }
 
     // let playerName = <span className="player-name"> { name }</span>;
-
     // if(isEditing)
     //     playerName = <input placeholder='Player Name' type="text"  required />
 
@@ -20,8 +20,8 @@ const Player = ({name, symbol}) => {
         <li>
             <span className="player">
             { !isEditing
-                ? <span className="player-name"> { name }</span>
-                : <input type="text" value={name} required />
+                ? <span className="player-name"> { playerName }</span>
+                : <input type="text" value={playerName} required />
             }
             <span className="payer-symbol">{ symbol }</span>
             </span>
